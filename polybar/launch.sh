c003a9d11eef04dafe310ab1473308bff1188ed9
Carlo Chech <carlo.chech@fibraweb.it>
2018-11-13 18:26:32 +0100

    addd_polybar

---
+++ polybar/launch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# Terminate any currently running instances
+killall -q polybar
+
+# Pause while killall completes
+while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
+
+polybar --reload top -c ~/.config/polybar/config &
+
+# Launch bar(s)
+# polybar dummy -q &
+# polybar top -q &
+# polybar bottom -q  &
+
+echo "polybars launched..."
