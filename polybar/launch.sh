ab59f07ad799b56c3cb9dfd33e9139c42358da84
wdog <wdog666@gmail.com>
2019-03-21 23:19:51 +0100

    simpler

--- polybar/launch.sh
+++ polybar/launch.sh
@@ -6,11 +6,8 @@ pkill -9 polybar
 # Pause while killall completes
 while pgrep -u $UID -x polybar > /dev/null; do sleep 1; done
 
-polybar --reload top -c ~/.config/polybar/config &
+polybar top -c ~/.config/polybar/config &
+polybar bottom -c ~/.config/polybar/config &
 
-# Launch bar(s)
-# polybar dummy -q &
-# polybar top -q &
-# polybar bottom -q  &
 
 echo "polybars launched..."
